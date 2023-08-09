4585a8bcca07c4d9c72d2255b483fabb2eeae188
Aslan Evloev <eevloeev@gmail.com>
2023-08-10 03:01:42 +0530

    api code refactoring

--- src/app/api/v1/events/route.ts
+++ src/app/api/v1/events/route.ts
@@ -1,4 +1,3 @@
-import { serializeEvents } from "@/app/utils/serializeEvents"
 import { USDT_ETH } from "@/constant/contracts"
 import { mongoClient } from "@/lib/mongodb"
 import { web3 } from "@/lib/web3"
@@ -6,34 +5,29 @@ import { NextRequest, NextResponse } from "next/server"
 import { EventLog } from "web3-eth-contract"
 
 export async function GET(request: NextRequest) {
-  const eventsCollection = mongoClient.db().collection("events")
+  const eventsCollection = mongoClient.db("db").collection("events")
 
   const eventsCount = await eventsCollection.countDocuments()
 
-  if (eventsCount > 0) {
-    const events = await eventsCollection.find().toArray()
+  if (eventsCount === 0) {
+    const contract = new web3.eth.Contract(USDT_ETH.abi, USDT_ETH.address)
 
-    return NextResponse.json({
-      count: events.length,
-      events,
-    })
+    const events = (await contract.getPastEvents(
+      // @ts-ignore
+      "Transfer",
+      {
+        fromBlock: 17612968,
+        toBlock: 17612970,
+      }
+    )) as EventLog[]
+
+    await eventsCollection.insertMany(events)
   }
 
-  const contract = new web3.eth.Contract(USDT_ETH.abi, USDT_ETH.address)
-
-  const events = (await contract.getPastEvents(
-    // @ts-ignore
-    "Transfer",
-    {
-      fromBlock: 17612968,
-      toBlock: 17612970,
-    }
-  )) as EventLog[]
-
-  eventsCollection.insertMany(events)
+  const events = await eventsCollection.find().toArray()
 
   return NextResponse.json({
     count: events.length,
-    events: serializeEvents(events),
+    events,
   })
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import { AppBar, Container, Toolbar, Typography } from "@mui/material"
+
 export default function Home() {
-  return <div>Test task</div>
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Container maxWidth="lg">
+            <Typography variant="h6" color="inherit" component="div">
+              USDT (ERC-20) &mdash; Transfer events
+            </Typography>
+          </Container>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="lg"></Container>
+    </div>
+  )
 }

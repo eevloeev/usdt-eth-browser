f61d0792d217ff3f0ef63ba2dc9bca1c0ad70b6f
Aslan Evloev <eevloeev@gmail.com>
2023-08-09 22:44:39 +0530

    install necessary dependencies
    - MUI
    - Redux Toolkit
    - web3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,23 @@
-export default function RootLayout({
-  children,
-}: {
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+
+import CssBaseline from "@mui/material/CssBaseline"
+
+type Props = {
   children: React.ReactNode
-}) {
+}
+
+export default function RootLayout(props: Props) {
+  const { children } = props
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <CssBaseline />
+        <div>{children}</div>
+      </body>
     </html>
   )
 }

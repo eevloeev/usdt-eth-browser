720c9200384ab2dffa14e31727475395d1112ebf
Aslan Evloev <eevloeev@gmail.com>
2023-08-10 18:23:33 +0530

    code refactoring

--- src/lib/web3.ts
+++ src/lib/web3.ts
@@ -1,7 +1,11 @@
 import Web3 from "web3"
 
-const INFURA_API_KEY = process.env.INFURA_API_KEY
+const key = process.env.INFURA_API_KEY
 
-const web3 = new Web3("https://mainnet.infura.io/v3/" + INFURA_API_KEY)
+if (typeof key !== "string") {
+  throw new Error("Please define the INFURA_API_KEY environment variable")
+}
+
+const web3 = new Web3("https://mainnet.infura.io/v3/" + key)
 
 export { web3 }
